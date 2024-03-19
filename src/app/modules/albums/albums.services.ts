ced1fe5f75dfe7d707c701d42c68ea6460c59317
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-03-20 01:24:43 +0600

    refactor: Updated DB query for retrieving one song with ID

--- src/app/modules/albums/albums.services.ts
+++ src/app/modules/albums/albums.services.ts
@@ -1,7 +1,7 @@
-const GET_ALBUMS_FROM_DB = "SELECT * FROM albums";
+
 const GET_ALBUM_BY_ID_FROM_DB = "SELECT * FROM albums WHERE album_uid = $1";
 
 export const AlbumsServices = {
-  GET_ALBUMS_FROM_DB,
+
   GET_ALBUM_BY_ID_FROM_DB,
 };
